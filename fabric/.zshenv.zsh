7758ddf2484d860a8b08257c932c1d5ae78c0e7f
Sven Sönnichsen <sven@sven-s.de>
2024-12-21 21:05:13 +0100

    Add fabric

---
+++ fabric/.zshenv.zsh
@@ -0,0 +1,18 @@
+# Loop through all files in the ~/.config/fabric/patterns directory
+for pattern_file in $HOME/.config/fabric/patterns/*; do
+    # Get the base name of the file (i.e., remove the directory path)
+    pattern_name=$(basename "$pattern_file")
+
+    # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+    alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+
+    # Evaluate the alias command to add it to the current shell
+    eval "$alias_command"
+done
+
+yt() {
+    local video_link="$1"
+    fabric -y "$video_link" --transcript
+}
+
+# vim: set ft=zsh ts=2 sw=2 et:
---
+++ fabric/path.zsh
@@ -0,0 +1,4 @@
+#Golang environment variables
+export GOROOT=$(brew --prefix go)/libexec
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH
