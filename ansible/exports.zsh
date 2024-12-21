14fdc26183d82fd273115b45fb8d3b2331afda3d
Sven Sönnichsen <sven@sven-s.de>
2024-12-21 21:30:06 +0100

    Remove ansible vaults

--- ansible/exports.zsh
+++ ansible/exports.zsh
@@ -4,8 +4,4 @@
 VAULT=${0%/*}/bin/vault-keychain-client
 
 export ANSIBLE_VAULT_ID_MATCH=true
-export ANSIBLE_VAULT_IDENTITY_LIST="agross@$VAULT,
-co-it@$VAULT,
-heco@$VAULT,
-kessler@$VAULT,
-gw@$VAULT"
+export ANSIBLE_VAULT_IDENTITY_LIST=""
